04ae7b7f497f4e563a61c86ba47f73e48b5f18e0
Tom LaMarre <tlamarre91+github@gmail.com>
2024-01-28 20:15:59 -0600

    feat: add NetworkFactory.demo()

--- model/network/NetworkFactory.ts
+++ model/network/NetworkFactory.ts
@@ -3,6 +3,48 @@ import { Agent } from "../agent";
 import { Positioned } from "@/model/types";
 
 export class NetworkFactory {
+  static demo() {
+    const network = NetworkFactory.grid(5, 5);
+
+    const nodes = [...network.nodesByName.values()];
+
+    const ZIGZAG_PROGRAM = `def start
+write right $f
+move $pf
+write i'm
+test here = $pf
+write here
+test here = $pf
+go l1
+
+def l1
+move down
+go start
+`;
+    const agent1 = Agent.fromCode("zigzaggy", ZIGZAG_PROGRAM);
+    network.addAgent(agent1, nodes[0]);
+
+    const CIRCLE_PROGRAM = `def start
+move right
+move right
+move right
+move down
+move down
+move down
+move left
+move left
+move left
+move up
+move up
+move up
+go start
+`;
+    const agent2 = Agent.fromCode("circleguy", CIRCLE_PROGRAM);
+    network.addAgent(agent2, nodes[1]);
+
+    return network;
+  }
+
   static grid(
     height: number,
     width: number,
